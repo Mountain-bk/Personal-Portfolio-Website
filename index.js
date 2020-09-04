7100346e30829f6610831b4e003bce5ecacea9a6
Ryo Yamamoto <67309797+Mountain-bk@users.noreply.github.com>
2020-09-04 20:14:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,278 +1,213 @@
-<!DOCTYPE html>
-<html lang="en" >
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://kit.fontawesome.com/9006e11a27.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="styles.css">
-    <link rel="icon" href="images/fabicon.png">
-    <title>Ryo Yamamoto Portfolio</title>
-  </head>
-  <body>
-    <section class="topContainer">
-      <div class="switchContainer">
-        <input type="checkbox" id="darkLight" class="darkLightSwitch">
-        <label for="darkLight" class="toggleDarkLight animation">
-          <i class="fas fa-moon"></i>
-          <i class="fas fa-sun"></i>
-          <div class="ball"></div>
-        </label>
-      </div>
-      <div class="main">
-        <div class="mainItem centerName animation">
-          <h2>My<br>Portfolio</h2>
-          <div class="top-links-row">
-            <div class="top-font">
-              <div class="top-item">
-                <a id="email" onmouseover="displayPop()" onmouseout="hidePop()" class="top-item-link" data-email="ryochaaaan0414@gmail.com" onclick="copyEmail()" target="_blank">
-                  <i class="fas fa-envelope"></i>
-                </a>
-              </div>
-              <div class="top-item">
-                <a class="top-item-link" href="https://twitter.com/home" target="_blank"><i class="fab fa-twitter-square"></i></a>
-              </div>
-              <div class="top-item">
-                <a class="top-item-link" href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-square"></i></a>
-              </div>
-              <div class="top-item">
-                <a class="top-item-link" href="https://www.linkedin.com/in/ryo-yamamoto-9190591a7/" target="_blank"><i class="fab fa-linkedin"></i></a>
-              </div>
-              <div class="top-item">
-                <a class="top-item-link" href="https://github.com/Mountain-bk" target="_blank"><i class="fab fa-github-square"></i></a>
-              </div>
-            </div>
-            <div class="top-copy-pop">
-              <div class="top-copy-email-text">
 
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="nav animation">
-        <ul class="nav-links">
-          <li><button class="a nav-link">About Me</button></li>
-          <li><button class="b nav-link">Skills</button></li>
-          <li><button class="c nav-link">Works</button></li>
-          <li><button class="d nav-link">Contact</button></li>
-        </ul>
-      </div>
-      <div class="scroll-down">
-        <div class="scroll-down-container">
-          <h4>scroll</h4>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-    </section>
-      <section class="secondContainer">
-        <div class="mainItem aboutMe">
-          <div class="title-aboutme animation">
-            <h2>About Me</h2>
-          </div>
-          <div class="text">
-            <div class="article">
-              <p>My name is Ryo Yamamoto. I'm currently working at a complex facility as a part-time worker. Since I took a python beginner class(Code in Place) provided by Stanford University in April, I got interest in computer science. Please check my CV if you got interest.</p>
-              <button onclick="goCV()" class="cv">CV(pdf)</button>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="thirdContainer">
-        <div class="mainItem skills animation">
-          <div class="hardSkills">
-            <div class="title-hardSkills">
-              <h2>Hard Skills</h2>
-            </div>
-            <div class="list-hardSkills">
-              <ul>
-                <li>Bilingual (Japanese and English)</li>
-                <li>Sales</li>
-                <li>Photoshop</li>
-                <li>python</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Javascript</li>
-              </ul>
-            </div>
-          </div>
-          <div class="softSkills">
-            <div class="title-softSkills">
-              <h2>Soft Skills</h2>
-            </div>
-            <div class="list-softSkills">
-              <ul>
-                <li>Willingness to learn</li>
-                <li>Visual Communication</li>
-                <li>Teamwork</li>
-                <li>Time management</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="fourthContainer">
-        <div class="fourthContainer-container">
-          <div class="works">
-            <div class="works-title">
-              <h2>Works</h2>
-            </div>
-            <div class="works-projects-container">
-              <div class="works-project">
-                <button class="information-link" type="button" value="Portfolio">
-                  <div class="project-image">
-                    <img class="portfolio" src="images/Portfolio Image.png" alt="">
-                  </div>
-                </button>
-                <div class="information-container">
-                  <button class="information-link" type="button" value="Portfolio" >
-                    <h3>Personal Portfolio Website</h3>
-                  </button>
-                </div>
-                <div class="github-box">
-                  <a class="github" href="https://github.com/Mountain-bk/Portfolio-Project-1" target="_blank">
-                    <div class="github-link">
-                      <i class="fab fa-github"></i>
-                      <h3>Github</h3>
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <div class="works-project">
-                <button class="information-link" type="button" value="Jappalo Wings">
-                  <div class="project-image">
-                    <img class="jappalo-wings" src="images/Jappalo Wings Top Page.png" alt="">
-                  </div>
-                </button>
-                <div class="information-container">
-                  <button class="information-link" type="button" value="Jappalo Wings">
-                    <div>
-                      <h3>Jappalo Wings(Delivery fast food restaurant imitation)</h3>
-                    </div>
-                  </button>
-                </div>
-                <div class="github-box">
-                  <a class="github" href="https://github.com/Mountain-bk/Portfolio-Project-1" target="_blank">
-                    <div class="github-link">
-                      <i class="fab fa-github"></i>
-                      <h3>Github</h3>
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <div class="works-project mainItem">
-                <button class="information-link" type="button" value="Netflix Original Comedy Movie Suggestion Program">
-                  <div class="project-image">
-                    <img class="netflix-comedy-movie-suggestor" src="images/Netflix Comedy Movie Suggestor.png" alt="">
-                  </div>
-                </button>
-                <div class="information-container">
-                  <button class="information-link" type="button" value="Netflix Original Comedy Movie Suggestion Program">
-                    <div>
-                      <h3>Netflix Original Comedy Movie Suggestion Program</h3>
-                    </div>
-                  </button>
-                </div>
-                <div class="github-box">
-                  <a class="github" href="https://github.com/Mountain-bk/Portfolio-Project-2" target="_blank">
-                    <div class="github-link">
-                      <i class="fab fa-github"></i>
-                      <h3>Github</h3>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="fifthContainer">
-        <div class="fifthContainer-container">
-          <div class="fifthContainer-content">
-            <div class="mainItem contactMe-title">
-              <h2>Contact</h2>
-            </div>
-            <div class="contactMe-links-container">
-              <div class="contactMe-links-row">
-                <div class="contact-font">
-                  <div class="item">
-                    <a class="item-link" href="https://twitter.com/home" target="_blank">
-                      <i class="fab fa-twitter-square"></i>
-                    </a>
-                  </div>
-                  <div class="item">
-                    <a class="item-link" href="https://www.facebook.com/" target="_blank">
-                      <i class="fab fa-facebook-square"></i>
-                    </a>
-                  </div>
-                  <div class="item">
-                    <a class="item-link" href="https://www.linkedin.com/in/ryo-yamamoto-9190591a7/" target="_blank">
-                      <i class="fab fa-linkedin"></i>
-                    </a>
-                  </div>
-                  <div class="item">
-                    <a class="item-link" href="https://github.com/Mountain-bk" target="_blank">
-                      <i class="fab fa-github-square"></i>
-                    </a>
-                  </div>
-                  <div class="item">
-                    <a id="emailContact" onmouseover="displayContactPop()" onmouseout="hideContactPop()" class="item-link" data-email="ryochaaaan0414@gmail.com" onclick="copyEmail()" target="_blank">
-                      <i class="fas fa-envelope"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div class="contact-copy-pop">
-                <div class="contact-copy-email-text">
+//Lists//
+const lightColors = [" ", "blue", "orange", "gold", "red"]
 
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <div class="modal">
-        <div class="modal-content-container">
-          <div class="close-btn-container">
-            <span class="close-btn">&times;</span>
-          </div>
-          <div class="modal-content">
-            <div class="title">
-              <div class="title-title">
-                <h3>Title:</h3>
-              </div>
-              <div class="title-text">
+const darkColors = ["dark-theme", "dark-theme bluedark", "dark-theme orangedark", "dark-theme golddark", "dark-theme reddark"]
 
-              </div>
-            </div>
-            <div class="url">
-              <div class="url-title">
-                <h3>URL/Link:</h3>
-              </div>
-              <div class="url-text">
+const mainItem = [...document.getElementsByClassName("mainItem")]
 
-              </div>
-            </div>
-            <div class="languages">
-              <div class="languages-title">
-                <h3>Languages:</h3>
-              </div>
-              <div class="languages-text">
+const sections = [...document.getElementsByTagName("section")]
 
-              </div>
-            </div>
-            <div class="production-period">
-              <div class="production-period-title">
-                <h3>Production Period:</h3>
-              </div>
-              <div class="production-period-text">
+const navLink = [...document.getElementsByClassName("nav-link")]
 
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    <script src="index.js"></script>
-  </body>
-</html>
+const darkItems = [...document.getElementsByClassName("darkItems")]
+
+//---Toggle Dark Mode Light Mode---//
+const checkbox = document.querySelector(".darkLightSwitch");
+checkbox.addEventListener("change", function(){
+  document.body.classList.toggle("dark-theme");
+});
+
+//---Change Background Color when Scroll Down---//
+window.addEventListener("scroll", function(){
+  changeBackgroundColor(document.body.className);
+})
+
+function changeBackgroundColor(key){
+  for (i=0; mainItem.length > i; i++){
+    var scrollTop = window.pageYOffset
+    if (scrollTop <= mainItem[i].offsetTop){
+      if (key == "dark-theme" || key == "dark-theme bluedark" || key == "dark-theme orangedark" || key == "dark-theme golddark" || key == "dark-theme reddark"){
+        document.body.className = darkColors[i]
+        break
+      } else{
+        document.body.className = lightColors[i]
+        break
+      }
+    }
+  }
+}
+
+
+//---Fade Contents and Change Background Color when Scroll Down---//
+const items = document.querySelectorAll(".animation")
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry =>{
+    if (entry.intersectionRatio > 0){
+      entry.target.classList.add("appear");
+    } else{
+      entry.target.classList.remove("appear");
+    }
+  })
+})
+
+items.forEach(item => {
+  observer.observe(item);
+})
+
+
+
+function scrollDown(key){
+  if (key == "a nav-link" || key == "a nav-link dark"){
+    window.scrollTo({top: sections[1].offsetTop, behavior: "smooth"});
+  } else if (key == "b nav-link" || key == "b nav-link dark"){
+    window.scrollTo({top: sections[2].offsetTop, behavior: "smooth"});
+  } else if (key == "c nav-link" || key == "c nav-link dark"){
+    window.scrollTo({top: sections[3].offsetTop, behavior: "smooth"});
+  } else if (key == "d nav-link" || key == "d nav-link dark"){
+    window.scrollTo({top: sections[4].offsetTop, behavior: "smooth"});
+  }
+}
+
+//---About Me page---//
+
+//Open CV(pdf) when Click Button//
+function goCV(){
+  open("CV-Ryo Yamamoto.pdf", "_blank");
+}
+
+//---Copy email address to clip board---//
+function copyEmail(){
+  var copyEmail = document.getElementById("email");
+  copyEmail = copyEmail.getAttribute("data-email");
+  var textArea = document.createElement("textarea");
+  textArea.value = copyEmail;
+  document.body.appendChild(textArea);
+  textArea.select();
+  textArea.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+}
+
+//---Display copying message when hover or click email font---//
+
+const emailFont = document.querySelector("#email");
+const emailFontContact = document.querySelector("#emailContact");
+const copyPop = document.querySelector(".top-copy-email-text");
+const copyPopContact = document.querySelector(".contact-copy-email-text");
+
+//---Top page---//
+function displayPop(){
+  copyPop.innerHTML = "Copy email address";//Display copy-pop when on mouse over email font//
+  copyPop.style.opacity = "1";
+}
+
+function hidePop(){
+  copyPop.style.opacity = "0";//Hide copy-pop when out mouse over email font//
+}
+
+emailFont.addEventListener("click", () =>{//Display message when you click email font//
+  copyPop.innerHTML = "Copied!";
+  copyPop.style.opacity = "1";
+  setTimeout(hidePop, 500);
+});
+
+//Scroll Down when click Nav//
+for (var i = 0; navLink.length > i; i++){
+  navLink[i].addEventListener("click", function(){
+    scrollDown(this.className);
+  })
+}
+
+//---Works Page---//
+
+//Open information of works//
+const infoBtn = document.querySelectorAll(".information-link");
+const modal = document.querySelector(".modal");
+const titleText = document.querySelector(".title-text");
+const languagesText = document.querySelector(".languages-text");
+const productionPeriodText = document.querySelector(".production-period-text");
+const urlText = document.querySelector(".url-text");
+
+for(let i = 0; infoBtn.length > i; i++){
+  infoBtn[i].addEventListener("click", () =>{
+    modal.style.visibility = "visible";
+    modal.style.opacity = "1";
+    if(infoBtn[i].value == "Portfolio"){
+      titleText.innerHTML = "<p>Personal Portfolio Website</p>";
+      urlText.innerHTML = "<a href='https://mountain-bk.github.io/Portfolio/' target='_blank'>https://mountain-bk.github.io/Portfolio/</a>"
+      languagesText.innerHTML =
+      "<div class='language-icon'>" +
+        "<i class='fab fa-html5'></i>" +
+        "<p>HTML</p>" +
+      "</div>" +
+      "<div class='language-icon'>" +
+        "<i class='fab fa-css3-alt'></i>" +
+        "<p>CSS</p>" +
+      "</div>" +
+      "<div class='language-icon'>" +
+        "<i class='fab fa-js'></i>" +
+        "<p>JavaScript</p>" +
+      "</div>";
+      productionPeriodText.innerHTML =
+      "<p>Since Jul 19th</p>" +
+      "<p>(Start learning HTML, CSS, JavaScript since Jun 22nd)</p>";
+    }else if(infoBtn[i].value == "Jappalo Wings"){
+      titleText.innerHTML = "<p>Jappalo Wings(Fast food imitation)</p>";
+      urlText.innerHTML = "<a href='https://mountain-bk.github.io/Portfolio-Project-1/' target='_blank'>https://mountain-bk.github.io/Portfolio-Project-1/</a>"
+      languagesText.innerHTML =
+      "<div class='language-icon'>" +
+        "<i class='fab fa-html5'></i>" +
+        "<p>HTML</p>" +
+      "</div>" +
+      "<div class='language-icon'>" +
+        "<i class='fab fa-css3-alt'></i>" +
+        "<p>CSS</p>" +
+      "</div>" +
+      "<div class='language-icon'>" +
+        "<i class='fab fa-js'></i>" +
+        "<p>JavaScript</p>" +
+      "</div>";
+      productionPeriodText.innerHTML =
+      "<p>Since Jul 30th</p>" +
+      "<p>(Start learning HTML, CSS, JavaScript since Jun 22nd)</p>";
+    }else if(infoBtn[i].value == "Netflix Original Comedy Movie Suggestion Program"){
+      titleText.innerHTML = "<p>Netflix Original Comedy Movie Suggestion Program</p>" +
+      "<p><a href='https://engineering.stanford.edu/news/free-coding-education-time-covid-19' target='_blank'>CS106A - Code in Place Final Project</a></p>";
+      urlText.innerHTML = "<a href='https://twitter.com/RyoYama37915576/status/1301489345308680192' target='_blank'>Demo Video</a>"
+      languagesText.innerHTML =
+      "<div class='language-icon'>" +
+        "<i class='fab fa-python'></i>" +
+        "<p>Python</p>" +
+      "</div>";
+      productionPeriodText.innerHTML =
+      "<p>Python Code(May 14th - May 24th)</p>" +
+      "<p>Tkinter Code(Aug 31st - Sep 4th)</p>";
+    }
+  });
+}
+
+
+//Close information of works//
+const closeBtn = document.querySelector(".close-btn");
+
+closeBtn.addEventListener("click", () =>{
+  modal.style.visibility = "hidden";
+  modal.style.opacity = "0";
+})
+
+//---Contact Page---//
+function displayContactPop(){
+  copyPopContact.innerHTML = "Copy email address";
+  copyPopContact.style.opacity = "1";
+}
+
+function hideContactPop(){
+  copyPopContact.style.opacity = "0";
+}
+
+emailFontContact.addEventListener("click", () =>{
+  copyPopContact.innerHTML = "Copied!";
+  copyPopContact.style.opacity = "1";
+  setTimeout(hideContactPop, 500);
+});
